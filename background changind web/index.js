c2c9e29fb471fb350aeba95fd445388be4346f56
Mohammad Talha <72750074+Mohammad-Talha-Faisal@users.noreply.github.com>
2022-11-18 21:29:45 +0500

    Add files via upload

---
+++ background changind web/index.js
@@ -0,0 +1,31 @@
+function hi() {
+
+    var bgc = document.getElementById("change");
+    var body = document.getElementById("color");
+
+    if (bgc.value == "red") {
+        body.style.backgroundColor = "red"
+    }
+
+    if (bgc.value == "blue") {
+        body.style.backgroundColor = "blue"
+    }
+
+    if (bgc.value == "green") {
+        body.style.backgroundColor = "green"
+    }
+
+    if (bgc.value == "black") {
+        body.style.backgroundColor = "black"
+    }
+
+    if (bgc.value == "violet") {
+        body.style.backgroundColor = "violet"
+    }
+
+    if (bgc.value == "indigo") {
+        body.style.backgroundColor = "indigo";
+    
+    }
+
+}
